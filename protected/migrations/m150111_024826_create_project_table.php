f2f86a8a8136d653117791e7b07dd6b0a3f9c4aa
Fellipe Vieira <fellipe.svieira@gmail.com>
2015-01-11 00:50:09 -0200

    Yii database migrations

---
+++ protected/migrations/m150111_024826_create_project_table.php
@@ -0,0 +1,25 @@
+<?php
+
+class m150111_024826_create_project_table extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m150111_024826_create_project_table does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
