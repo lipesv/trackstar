c970f7f64a6a9484fea288db8f84451908d5037d
lipesv <fellipe.svieira@gmail.com>
2015-02-02 11:22:14 -0200

    Adding RBAC roles to users on a per-project basis create command executed

--- protected/migrations/m150202_123950_add_role_to_tbl_project_user_assignment.php
+++ protected/migrations/m150202_123950_add_role_to_tbl_project_user_assignment.php
@@ -1,25 +1,27 @@
 <?php
 
-class m150202_123950_add_role_to_tbl_project_user_assignment extends CDbMigration
-{
-	public function up()
-	{
+class m150202_123950_add_role_to_tbl_project_user_assignment extends CDbMigration {
+	
+	public function up() {
+		$this->addColumn ( 'tbl_project_user_assignment', 'role', 'varchar(64)' );
+		
+		// the tbl_project_user_assignment.role is a reference to tbl_auth_item.name
+		$this->addForeignKey ( 'fk_project_user_role', 'tbl_project_user_assignment', 'role', 'tbl_auth_item', 'name', 'CASCADE', 'CASCADE' );
 	}
-
-	public function down()
-	{
-		echo "m150202_123950_add_role_to_tbl_project_user_assignment does not support migration down.\n";
-		return false;
+	
+	public function down() {
+		$this->dropForeignKey ( 'fk_project_user_role', 'tbl_project_user_assignment' );
+		$this->dropColumn ( 'tbl_project_user_assignment', 'role' );
 	}
-
+	
 	/*
-	// Use safeUp/safeDown to do migration with transaction
-	public function safeUp()
-	{
-	}
-
-	public function safeDown()
-	{
-	}
-	*/
+	 * // Use safeUp/safeDown to do migration with transaction
+	 * public function safeUp()
+	 * {
+	 * }
+	 *
+	 * public function safeDown()
+	 * {
+	 * }
+	 */
 }
