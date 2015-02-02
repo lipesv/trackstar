dc2744a1e1dc0f97cebf409b52f8772e33464309
lipesv <fellipe.svieira@gmail.com>
2015-02-02 10:41:23 -0200

    Adding RBAC roles to users on a per-project basis

---
+++ protected/migrations/m150202_123950_add_role_to_tbl_project_user_assignment.php
@@ -0,0 +1,25 @@
+<?php
+
+class m150202_123950_add_role_to_tbl_project_user_assignment extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m150202_123950_add_role_to_tbl_project_user_assignment does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
