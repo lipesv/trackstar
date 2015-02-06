a1a82d46fb2d3cb7843b924a72f61cf0e28e6379
lipesv <fellipe.svieira@gmail.com>
2015-02-06 10:53:33 -0200

    creating a new table to house our comments.

---
+++ protected/migrations/m150206_125124_create_user_comments_table.php
@@ -0,0 +1,25 @@
+<?php
+
+class m150206_125124_create_user_comments_table extends CDbMigration
+{
+	public function up()
+	{
+	}
+
+	public function down()
+	{
+		echo "m150206_125124_create_user_comments_table does not support migration down.\n";
+		return false;
+	}
+
+	/*
+	// Use safeUp/safeDown to do migration with transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+	*/
+}
